2d753b357398e6fbd574d4907933bb8f71df8466
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2024-01-31 00:07:21 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ContactsApp"
+include(":app")
+include(":core")
+include(":remote_data")
+include(":contacts_list_screen")
+include(":contact_details")
